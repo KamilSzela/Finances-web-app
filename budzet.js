6ed2b5756e63d5219739174e3501933058ccb1fc
Kamil Szela <kamil.szela.programista@gmail.com>
2019-10-06 20:33:10 +0200

    first positioning of site, display menu after click

---
+++ budzet.js
@@ -0,0 +1,11 @@
+$('#register').on('click', function(){
+	//register();
+	showMenu();
+	
+});
+
+function showMenu()
+{
+	$('.register').css('display','none');
+	$('.menu').css('display', 'block');
+}
