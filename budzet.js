81430566b450a9369e9e7b08d70c5b0e3a031759
Kamil Szela <kamil.szela.programista@gmail.com>
2019-10-31 18:13:47 +0100

    added expence site mechanics

--- budzet.js
+++ budzet.js
@@ -1,8 +1,10 @@
 
 var usersObj = [];
+var expencesObj = [];
 var lastUserID = 0;
 var loggedUserID = 0;
 var checkedID = 0;
+var lastExpenceID = 0;
 
 $(document).ready(function(){
 	
@@ -31,6 +33,24 @@ $('#expences').on('click',function(){
 	showExpenceManager();
 });
 
+$('#addExpenceButton').on('click',function(){
+	addNewExpence();
+});
+
+$('#escape').on('click', function(){
+	loggedUserID = 0;
+	var endOfArrayExpences = expencesObj.length;
+	expencesObj.splice(0,endOfArrayExpences);
+	location.reload();
+	$("#name").val("");
+	$("#password").val("");
+	$("#email").val("");
+});
+
+$('#showStorage').on('click', function(){
+	showExpenceStorage();
+});
+
 function loadUsersFromLocalStorage()
 {
 	if(localStorage.length>=1){
@@ -41,7 +61,8 @@ function loadUsersFromLocalStorage()
 	
 		usersObj.sort(function(a, b){return a.id - b.id;});
 		var lastPosition = usersObj.length;
-		lastUserID = usersObj[lastPosition-1].id;
+		if(lastPosition>0)
+			lastUserID = usersObj[lastPosition-1].id;
 	}
 }
 function loadUserstoArray(i)
@@ -151,6 +172,7 @@ function singUserIn()
 		loggedUserID = checkedID;
 		alert("You have been successfully logged in!");
 		//sessionStorage.setItem("UserID", loggedUserID);
+		loadExpencesOfLoggedUser();
 		showMenu();
 	}
 	else alert("This login dosen't exist");
@@ -197,4 +219,135 @@ function showExpenceManager()
 	$('.expenceContainer').css('display','block');
 	$('.register').css('display','none');
 	$('.menu').css('display', 'none');
+}
+function loadExpencesOfLoggedUser()
+{
+	for(var i=0; i<localStorage.length; i++){
+		loadExpencestoArray(i);
+	}
+
+	expencesObj.sort(function(a, b){return a.id - b.id;});
+	
+}
+function loadExpencestoArray(i)
+{
+	var nameOfValue = localStorage.key(i); 
+	if(nameOfValue.charAt(0)=='E'){	
+		var valueOfName = localStorage.getItem(nameOfValue);
+		getExpenceDataFromStringWithDashes(valueOfName);
+	}
+}
+function getExpenceDataFromStringWithDashes(valueOfName)
+{
+	var dashCounter = 0;
+	var string = "";
+	var ExpenceInArray = {
+	id: 0,
+	userId: 0,
+	amount: "",
+	date: "",
+	payment: "",
+	source: "",
+	comment: ""
+};
+	for(var i=0; i<valueOfName.length; i++)
+	{
+		if(valueOfName.charAt(i) != "/"){
+			string = string + valueOfName.substr(i,1);
+		}
+		if(valueOfName.charAt(i) == '/')
+		{
+	
+			switch(dashCounter)
+			{
+				case 0: ExpenceInArray.id = parseInt(string); 
+					if(lastExpenceID<ExpenceInArray.id)
+						lastExpenceID = ExpenceInArray.id; 
+					
+					string = ""; break;
+				case 1: ExpenceInArray.userId = parseInt(string); string = ""; 
+					if(ExpenceInArray.userId != loggedUserID) return;
+				break;
+				case 2: ExpenceInArray.amount = parseInt(string); string = ""; break;
+				case 3: ExpenceInArray.date = string; string = ""; break;
+				case 4: ExpenceInArray.payment = string; string = ""; break;
+				case 5: ExpenceInArray.source = string; string = ""; break;
+				case 6: ExpenceInArray.comment = string; string = ""; break;
+			}
+			dashCounter++;
+		}
+		if (dashCounter == 7)
+		{
+			expencesObj.push(ExpenceInArray);
+		}
+	}
+}
+function addNewExpence()
+{
+	
+	lastExpenceID++;
+	var ExpenceInArray = {
+		id: 0,
+		userId: 0,
+		amount: "",
+		date: "",
+		payment: "",
+		source: "",
+		comment: ""
+	};
+	var amount;
+	amount = $('#amount').val();
+	var date;
+	date = $('#dateExpence').val();
+	var wayOfPayment;
+	wayOfPayment = $("input[type=radio][name=payment]:checked").val();
+	var category;
+	category = $("input[type=radio][name=cat]:checked").val();
+	var comment;
+	comment = $('#commentExpence').val();
+	var string = lastExpenceID.toString()+"/"+ loggedUserID.toString()+"/"+ amount +"/"+date+"/"+wayOfPayment+"/"+category+"/"+comment+"/";
+	
+	
+	ExpenceInArray.id = lastExpenceID;
+	ExpenceInArray.userId = loggedUserID;
+	ExpenceInArray.amount = amount;
+	ExpenceInArray.date = date;
+	ExpenceInArray.payment = wayOfPayment;
+	ExpenceInArray.source = category;
+	ExpenceInArray.comment = comment;
+	expencesObj.push(ExpenceInArray);
+	
+	var nameOfExpence = "Expence" + lastExpenceID.toString();
+	var valueOfExpence = string;
+	localStorage.setItem(nameOfExpence, valueOfExpence);
+	
+	alert("Dodałeś nowy wydatek!");
+	
+	$('#amount').val("");
+	$('#dateExpence').val("");
+	$('#commentExpence').val("");
+	
+}
+function showExpenceStorage(){
+	var expenceData = "";
+	
+	for(var i=0; i<expencesObj.length; i++){
+		expenceData = expenceData + addExpenceData(i) + "</br>";
+	}
+	
+	if(expenceData == "") $('.expenceContainer').html("niestety expencesObj jest puste");
+	
+	$('.expenceContainer').html(expenceData);
+}
+function addExpenceData(i)
+{
+	var string = "";
+	string = string +" "+ expencesObj[i].id.toString();
+	string = string +" "+ expencesObj[i].userId.toString();
+	string = string +" "+ expencesObj[i].amount;
+	string = string +" "+ expencesObj[i].date;
+	string = string +" "+ expencesObj[i].payment;
+	string = string +" "+ expencesObj[i].source;
+	string = string +" "+ expencesObj[i].comment;
+	return string;
 }
